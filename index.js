6f480d37d48b1a3c2c62c1625107bd162f70402a
ThayPedroso <ThayPedroso>
2020-04-18 20:47:25 -0300

    QueueFrontier
    QueueFrontier extends StackFrontier using Javascript inheritance.

--- index.js
+++ index.js
@@ -11,36 +11,71 @@ function Node (state, parent, action) {
 
 function StackFrontier () {
     let frontier = []
-    
-    return {
-        frontier,
-        add(node) {
-            frontier.push(node)
-        },
-        contains_state(state) {
-            for (node of frontier) {
-                node.state === state ? true : false
-            }
-        },
-        empty() {
-            return frontier.length() === 0
-        },
-        remove() {
-            if(empty()) {
-                console.log('empty frontier')
-            } else {
-                node = frontier[frontier.length - 1]
-                frontier = frontier.pop()
-                return node
-            }
-        }
+    this.frontier = frontier
+}
+
+StackFrontier.prototype.add = function(node) {
+    this.frontier.push(node)
+}
+
+StackFrontier.prototype.contains_state = function(state) {
+    for (node of this.frontier) {
+        if (node.state[0] === state[0] && node.state[1] === state[1]) return true 
+    }
+    return false
+}
+
+StackFrontier.prototype.empty = function() {
+    return frontier.length === 0
+}
+
+StackFrontier.prototype.remove = function() {
+    if(this.empty()) {
+        console.log('empty frontier')
+    } else {
+        node = frontier[frontier.length - 1]
+        this.frontier.pop()
+        return node
+    }
+}
+
+function QueueFrontier() {
+    StackFrontier.call(this)
+}
+
+// QueueFrontier extends StackFrontier
+QueueFrontier.prototype = Object.create(StackFrontier.prototype)
+QueueFrontier.prototype.constructor = QueueFrontier
+
+QueueFrontier.prototype.remove = function() {
+    if(this.empty()) {
+        console.log('empty frontier')
+    } else {
+        node = frontier[0]
+        this.frontier.shift()
+        return node
     }
 }
 
 const start = Node([1,1], undefined, undefined)
-console.log(start)
-let frontier = StackFrontier()
+const finish = Node([2,2], undefined, undefined)
+// console.log(start)
+let frontier = new StackFrontier()
+frontier.add(finish)
 frontier.add(start)
-
-console.log(frontier.contains_state([1,1]))
 console.log(frontier)
+
+console.log(frontier.contains_state([2,1]))
+frontier.remove()
+console.log(frontier)
+console.log(frontier.contains_state([1,1]))
+
+let bfsFrontier = new QueueFrontier()
+bfsFrontier.add(start)
+bfsFrontier.add(finish)
+console.log(bfsFrontier)
+console.log(bfsFrontier.contains_state([1,1]))
+console.log(bfsFrontier.contains_state([2,2]))
+bfsFrontier.remove()
+console.log(bfsFrontier.frontier[0].state)
+console.log(bfsFrontier.contains_state([2,2]))
